869aa42f7d8da2ba56db94ceb2da47fe36b5af58
Samy Rabih <contact@lahaut.info>
2014-06-15 12:45:50 +0200

    Données 1er slide

---
+++ assets/script.js
@@ -0,0 +1,35 @@
+ var socket = io('http://192.168.100.57:3000');
+ socket.on('story event', function(msg){
+	console.log(msg);
+	if (typeof slides_map[msg] != "undefined")
+	{
+		displaySlide(slides_map[msg]);
+	}
+});
+var slides;
+var viewportHeight = $(window).height();
+$(document).ready(function($){
+  $.getJSON("/assets/data.json", function(data){
+    slides = data.slides;
+	slides_map = data.slides_map;
+	console.log(slides[0].image);
+	$('#container').height(viewportHeight);
+	$('#container').css('background-image', "url("+slides[0].image+")");
+  });
+});
+
+function displaySlide(id)
+{
+	slide = slides[id];
+	if (typeof slide.page != "undefined")
+	{
+		if (!slide.page)
+			$('#content').html('');
+		else
+			$( "#content" ).load(slide.page);
+	}
+	if (!slide.image)
+		$('#container').css('background-image', 'none');
+	else
+		$('#container').css('background-image', "url("+slide.image+")");
+}
