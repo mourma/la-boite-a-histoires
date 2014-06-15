1c04553fe256ba12f739bb374902745e868de56f
Samy Rabih <contact@lahaut.info>
2014-06-15 10:37:07 +0200

    On écoute sur le port 3000 On commence à envoyer des évènements

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
 var keypress = require('keypress');
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendfile('index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
 
 // make `process.stdin` begin emitting "keypress" events
 keypress(process.stdin);
@@ -7,6 +22,7 @@ keypress(process.stdin);
 process.stdin.on('keypress', function (ch, key) {
   if (key && key.name == 'c') {
 	console.log('toto');
+	io.emit('story event', 'test');
   }
   if (key && key.ctrl && key.name == 'c') {
     process.stdin.pause();
