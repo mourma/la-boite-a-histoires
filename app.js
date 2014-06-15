26d5e5950983f880b0d1df8f3dbcced2ce2f8e10
Samy Rabih <contact@lahaut.info>
2014-06-15 09:59:08 +0200

    Premier script pour détecter les frappes de touche

---
+++ app.js
@@ -0,0 +1,17 @@
+var keypress = require('keypress');
+
+// make `process.stdin` begin emitting "keypress" events
+keypress(process.stdin);
+
+// listen for the "keypress" event
+process.stdin.on('keypress', function (ch, key) {
+  if (key && key.name == 'c') {
+	console.log('toto');
+  }
+  if (key && key.ctrl && key.name == 'c') {
+    process.stdin.pause();
+  }
+});
+
+process.stdin.setRawMode(true);
+process.stdin.resume();
